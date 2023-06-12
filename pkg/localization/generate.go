c064adbc6227c3d9535c1d84d023f6f49796ea66
Christoph Krassnigg <christoph.krassnigg@icloud.com>
2023-06-12 18:45:30 +0200

    feature: added localized storage

--- pkg/localization/generate.go
+++ pkg/localization/generate.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	. "localizer/pkg/env"
 	"localizer/pkg/file"
+	"localizer/pkg/logger"
 	"localizer/pkg/translator"
 	"os"
 )
@@ -13,6 +14,12 @@ func Update(path string) error {
 	if err != nil {
 		return err
 	}
+	// error ignored, if file doesnt exist
+	added, _ := ReadFileMap(yamlPath(path, "added"))
+	if added == nil {
+		added = map[string]string{}
+	}
+
 	source, err := ReadFileMap(yamlPath(path, SourceLanguage))
 	if err != nil {
 		return err
@@ -27,58 +34,67 @@ func Update(path string) error {
 		return err
 	}
 
-	for _, language := range TargetLanguagesDeepL {
-		target := map[string]string{}
-		if _, err = os.Stat(yamlPath(path, language)); err == nil && !errors.Is(err, os.ErrNotExist) {
-			target, err = ReadFileMap(yamlPath(path, language))
-			if err != nil {
-				return err
-			}
+	for k, v := range add {
+		if added[k] == v {
+			continue
 		}
+		logger.Debug.Printf("Translating \"%s\"", k)
+		for _, language := range TargetLanguagesDeepL {
+			target := map[string]string{}
+			if _, err = os.Stat(yamlPath(path, language)); err == nil && !errors.Is(err, os.ErrNotExist) {
+				target, err = ReadFileMap(yamlPath(path, language))
+				if err != nil {
+					return err
+				}
+			}
 
-		for k, v := range add {
 			translated, err := translator.TranslateDeepL(v, SourceLanguage, language)
 			if err != nil {
 				return err
 			}
 			target[k] = translated.Translations[0].Text
-		}
 
-		err = writeFileMap(yamlPath(path, language), target)
-		if err != nil {
-			return err
+			err = writeFileMap(yamlPath(path, language), target)
+			if err != nil {
+				return err
+			}
 		}
-	}
-	for _, language := range TargetLanguagesGoogle {
-		target := map[string]string{}
-		if _, err := os.Stat(yamlPath(path, language)); err == nil && !errors.Is(err, os.ErrNotExist) {
-			target, err = ReadFileMap(yamlPath(path, language))
+		for _, language := range TargetLanguagesGoogle {
+			target := map[string]string{}
+			if _, err := os.Stat(yamlPath(path, language)); err == nil && !errors.Is(err, os.ErrNotExist) {
+				target, err = ReadFileMap(yamlPath(path, language))
+				if err != nil {
+					return err
+				}
+			}
+
+			toTranslateKey := make([]string, 0, len(add))
+			toTranslateValue := make([]string, 0, len(add))
+
+			toTranslateKey = append(toTranslateKey, k)
+			toTranslateValue = append(toTranslateValue, v)
+
+			translations, err := translator.TranslateGoogle(toTranslateValue, SourceLanguage, language)
+			if err != nil {
+				return err
+			}
+
+			for i, translation := range translations {
+				target[toTranslateKey[i]] = translation.Text
+			}
+
+			err = writeFileMap(yamlPath(path, language), target)
 			if err != nil {
 				return err
 			}
 		}
 
-		toTranslateKey := make([]string, 0, len(add))
-		toTranslateValue := make([]string, 0, len(add))
-
-		for k, v := range add {
-			toTranslateKey = append(toTranslateKey, k)
-			toTranslateValue = append(toTranslateValue, v)
-		}
-
-		translations, err := translator.TranslateGoogle(toTranslateValue, SourceLanguage, language)
+		added[k] = v
+		err = writeFileMap(yamlPath(path, "added"), added)
 		if err != nil {
 			return err
 		}
 
-		for i, translation := range translations {
-			target[toTranslateKey[i]] = translation.Text
-		}
-
-		err = writeFileMap(yamlPath(path, language), target)
-		if err != nil {
-			return err
-		}
 	}
 
 	err = file.Clear(yamlPath(path, "add"))
